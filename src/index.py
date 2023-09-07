0c8667ae509b96ee125bcd09ed2449358bf1b5a1
Garubieru <gabrielmorishita23@gmail.com>
2023-09-06 23:34:56 -0300

    feat: ensure yolo is trained for people recognition

--- src/index.py
+++ src/index.py
@@ -1,31 +1,42 @@
-from typing import TypedDict
+import os
 from model.yolo_model import YoloModel
-from types import SimpleNamespace
 from PIL import Image
-import argparse
+from tap import Tap
+
+
+class YoloPeopleRecognitionArgs(Tap):
+    use_pre_trained: bool = True
+    model: str = 'yolov8n.pt'
+    train_folder: str = 'train'
+    dataset: str = 'data.yaml'
+    image_name: str
+    epochs: int = 3
+
+
+def get_file_path(file_name: str) -> str:
+    return os.path.join(os.getcwd(), file_name)
 
 
 if __name__ == '__main__':
-    argument_parser = argparse.ArgumentParser('yolo_face_recognition')
-    argument_parser.add_argument(
-        '--use_pre_trained', type=bool, help='Set model to use a pre trained if is not trained')
+    arguments = YoloPeopleRecognitionArgs('yolo_face_recognition').parse_args()
 
-    arguments = argument_parser.parse_args()
-
-    yolo_v8_model_file = 'yolov8n.pt'
-    dataset_path = 'coco128.yaml'
+    dataset_path = get_file_path(arguments.dataset)
 
     model = YoloModel({
-        'model_name': yolo_v8_model_file,
+        'model_name': arguments.model,
         'dataset_path': dataset_path
     })
 
-    model.train(4, arguments.use_pre_trained)
+    model.train(arguments.train_folder, arguments.epochs,
+                arguments.use_pre_trained)
 
-    results = model.predict('https://ultralytics.com/images/bus.jpg')
+    results = model.predict(get_file_path(
+        f'images/{arguments.image_name}'))
+
+    model.evaluate()
 
     for result in results:
         im_array = result.plot()
         im = Image.fromarray(im_array[..., ::-1])
         im.show()
-        im.save('results/results.jpg')
+        im.save(get_file_path(f'results/{arguments.image_name}'))
